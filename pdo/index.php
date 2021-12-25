23a097c1803abbad267d2310b7767a454461b0d6
mjavier1 <mjaguirre5@gmail.com>
2021-12-25 14:39:23 -0400

    php
    proyecto pdo

---
+++ pdo/index.php
@@ -0,0 +1,11 @@
+<?php
+///var/www# sudo chmod -R 777 pdo
+//caundo creo proyecto nuevo tengo darle permiso
+/*
+mi nombre marcos Aguirre 
+proyecto practica pdo 
+udemy
+*/
+echo("helluuuof");
+
+?>
